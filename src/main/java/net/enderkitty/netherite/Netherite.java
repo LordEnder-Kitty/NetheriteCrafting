6bb66243a857d12d6c75e188f67f63c9090d7398
LordEnder_Kitty <dragonlancenerd@gmail.com>
2022-07-13 23:17:26 -0500

    Initial commit

---
+++ src/main/java/net/enderkitty/netherite/Netherite.java
@@ -0,0 +1,15 @@
+package net.enderkitty.netherite;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Netherite implements ModInitializer {
+	public static final String MOD_ID = "netherite";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
